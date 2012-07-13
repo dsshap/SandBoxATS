ff481bf57fe70b98b9a82bdd31bb835887efd603
Dave Shapiro <dss.shapiro@gmail.com>
2012-07-13 13:48:46 -0500

    adding lock for production env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,10 @@
 SandBoxATS::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
+	config.middleware.insert_after(::Rack::Lock, "::Rack::Auth::Basic", "Staging") do |u, p|
+    [u, p] == ['sandbox', 'sandbox213']
+  end
+
   # Code is not reloaded between requests
   config.cache_classes = true
 
